a7d1c5d484a2d1d472b87c4b9621f82a81a4658d
Larissa Gabrielle Fagundes Andrade <larissa.gabrielle@alunoescolassociais.org.br>
2024-07-04 12:12:58 -0300

    terminando o codigo por hoje com a função do nome ficar salvo no console.

--- controller.js
+++ controller.js
@@ -18,7 +18,8 @@ function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
 
     if(nomeUser){
-
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
     }else{
         alert("Favor, informe um nome para cadastro.");
     }
