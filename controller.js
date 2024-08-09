f965bf299db644bbb152c1f5fda7fcf509a89d21
Larissa Gabrielle Fagundes Andrade <larissa.gabrielle@alunoescolassociais.org.br>
2024-08-09 07:13:34 -0300

    função para exluir e editar os nomes da lista criada/editada.

--- controller.js
+++ controller.js
@@ -13,14 +13,16 @@ function acessar(){
 }
 
 // Função para armazenar os nomes em ARRAY
-var dadosLista = []; //Está em branco porque assim cabe mais coisas.
+var dadosLista = []; //Está em branco porque assim quanto mais coisa eu colocar, cabe normalmente.
 
 function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
 
     if(nomeUser){
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
+        //console.log(dadosLista); Transformei em comentário porque não será mais necessário. Criado para teste.
+        criaLista();
+        document.getElementById('nomeUser').value = ""; //Linha criada para o campo "Nome" ficar em branco após apertar o botão "Salvar".
     }else{
         alert("Favor, informe um nome para cadastro.");
     }
@@ -29,7 +31,20 @@ function salvarUser(){
 function criaLista(){
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário.</th><th>Ações.</th></tr>"; //Código pego no cadastro.html
     for(let i=0;i <= (dadosLista.length -1);i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>"; //Linha criada para o botão "Editar" funcionar.
+        document.getElementById('tabela').innerHTML = tabela;
     }
 
+}
+
+//Função para editar os nomes das listas.
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1)   //Altera o conteúdo de uma lista, adicionando novos elementos enquanto remove elementos antigos.
+}
+
+// Função para excluir o nome da lista.
+function excluir(i){
+    dadosLista.splice((i - 1), 1); //Remove o nome do ARRAY.
+    document.getElementById('tabela').deleteRow(i); //Deleta o nome da tabela.
 }
